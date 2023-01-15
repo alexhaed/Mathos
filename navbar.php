3d14aa2405a31e77875c0f2a9530d32043866c7d
Alexandre Haederli <alexhaederli@gmail.com>
2023-01-15 22:02:06 +0100

    V 0.1

---
+++ navbar.php
@@ -0,0 +1,7 @@
+		<nav class="navtop">
+			<div>
+				<h1>Mathos</h1>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Quitter</a>
+				<a href="index.php"><i class="fas fa-user-circle"></i>Accueil</a>
+			</div>
+		</nav>
