a48c45a6ec670deccb4ad794078a494c091c1105
Alexandre Haederli <alexhaederli@gmail.com>
2023-01-22 19:42:06 +0100

    Minor fixes

--- admin/stats.php
+++ admin/stats.php
@@ -1,7 +1,5 @@
 <?php
-// We need to use sessions, so you should always start sessions using the below code.
 session_start();
-// If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin']) || $_SESSION['admin'] != 1) {
 	header('Location: ../login.html');
 	exit;
@@ -34,11 +32,11 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['admin'] != 1) {
 			</div>
 		</nav>
 <?php
-		include '../mysql_login.php';
-		$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-		if (mysqli_connect_errno()) {
-			echo "Failed to connect to MySQL: ".mysqli_connect_error();
-		}
+include '../mysql_login.php';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: ".mysqli_connect_error();
+}
 ?>
 		<div class="content">
 			<h2>Statistiques</h2>
@@ -46,87 +44,89 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['admin'] != 1) {
 					<label for="user-select"><i class="fa-solid fa-user"></i> Choisir un utilisateur:</label>
 					<select name="user" id="user-select" onChange="change(this.value);">
 <?php
-	
-	if (isset($_GET['userid'])) $id = $_GET['userid'];
-	else $id = $_SESSION['id'];
+if (isset($_GET['userid'])) $id = $_GET['userid'];
+else $id = $_SESSION['id'];
 
-	if ($users = mysqli_query($con, "SELECT id, username FROM accounts ORDER BY id ASC")) {
-	    while ($user = mysqli_fetch_array($users)) {  
-	    	echo '<option value="'.$user[0].'" ';
-	    	if ($user[0] == $id) echo 'selected';
-	    	echo '>'.$user[1].'</option>';
-	    }
-	} else {
-	    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+if ($users = mysqli_query($con, "SELECT id, username FROM accounts ORDER BY id ASC")) {
+    while ($user = mysqli_fetch_array($users)) {  
+    	echo '<option value="'.$user[0].'" ';
+    	if ($user[0] == $id) echo 'selected';
+    	echo '>'.$user[1].'</option>';
+    }
+} else {
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+}
+echo "</select><br><br>";
+
+if ($exercices = mysqli_query($con, "SELECT SUM(`reussis`) AS C FROM scores WHERE userid = ".$id)) {
+	$row = $exercices->fetch_assoc();
+	if ($row["C"] == 0) {
+		echo "Tu n'as fait aucun exercice pour l'instant. Reviens plus tard!";
+		mysqli_close($con);
+		exit();
 	}
-	echo "</select><br><br>";
+	else echo "<i class='fa-solid fa-fire'></i> Total des calculs réussis: <b>".$row["C"]."</b><br><br>";
+} else {
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+}
 
-	if ($exercices = mysqli_query($con, "SELECT SUM(`reussis`) AS C FROM scores WHERE userid = ".$id)) {
-		$row = $exercices->fetch_assoc();
-		if ($row["C"] == 0) {
-			echo "Tu n'as fait aucun exercice pour l'instant. Reviens plus tard!";
-			mysqli_close($con);
-			exit();
-		}
-    	else echo "<i class='fa-solid fa-fire'></i> Total des calculs réussis: <b>".$row["C"]."</b><br><br>";
-	} else {
-	    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+if ($duree = mysqli_query($con, "SELECT SUM(`temps`) AS C FROM scores WHERE userid = ".$id)) {
+	$row = $duree->fetch_assoc();
+	if ($row["C"] == 0) {
+		echo "Tu n'as fait aucun exercice pour l'instant. Reviens plus tard!";
+		mysqli_close($con);
+		exit();
 	}
-
-	if ($duree = mysqli_query($con, "SELECT SUM(`temps`) AS C FROM scores WHERE userid = ".$id)) {
-		$row = $duree->fetch_assoc();
-		if ($row["C"] == 0) {
-			echo "Tu n'as fait aucun exercice pour l'instant. Reviens plus tard!";
-			mysqli_close($con);
-			exit();
-		}
-    	else {
-    		$minutes = floor($row["C"] / 60);
-    		//$secondes = $row["C"] % 60;
-    		echo "<i class='fa-solid fa-hourglass-end'></i> Tu as calculé pendant ".$minutes." minute";
-    		if($minutes > 1) echo "s";
-    		//echo " et ".$secondes." seconde";
-    		//if($secondes > 1) echo "s";
-    		echo ".<br><br>";
-    	}
-	} else {
-	    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+	else {
+		$minutes = floor($row["C"] / 60);
+		//$secondes = $row["C"] % 60;
+		echo "<i class='fa-solid fa-hourglass-end'></i> Tu as calculé pendant ".$minutes." minute";
+		if($minutes > 1) echo "s";
+		//echo " et ".$secondes." seconde";
+		//if($secondes > 1) echo "s";
+		echo ".<br><br>";
 	}
+} else {
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+}
 
-	if ($result = mysqli_query($con, "SELECT `exercice`, COUNT(*) AS C FROM scores WHERE userid = ".$id." GROUP BY `exercice` ORDER BY C DESC;")) {
-	    if (mysqli_num_rows($result) > 0) {
-	        echo "<i class='fa-brands fa-gratipay'></i> Exercices préférés:<br>";
-	        while ($row = mysqli_fetch_array($result)) {
-            	echo "&nbsp;&nbsp;&nbsp;&nbsp;<i class='fa-solid fa-angle-right'></i> ";
-                switch($row['exercice']) {
-                	case "addsous":
-                		echo "Addition et soustraction";
-                		break;
-                	case "compl":
-                		echo "Compléments";
-                		break;
-                	case "multidiv":
-                		echo "Multiplication et division";
-                		break;
-                	case "prio":
-                		echo "Priorité des opérations";
-                		break;
-                	case "addrelatifs":
-                		echo "Nombres entiers relatifs";
-                		break;
-               }
-               echo	": ".$row['C']." fois<br>";
-	        }
-	        echo "<br>";
-	        mysqli_free_result($result);
-	    } else {
-	        echo "No records matching your query were found.";
-	    }
-	} else {
-	    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
-	}
+if ($result = mysqli_query($con, "SELECT `exercice`, COUNT(*) AS C FROM scores WHERE userid = ".$id." GROUP BY `exercice` ORDER BY C DESC;")) {
+    if (mysqli_num_rows($result) > 0) {
+        echo "<i class='fa-brands fa-gratipay'></i> Exercices préférés:<br>";
+        while ($row = mysqli_fetch_array($result)) {
+        	echo "&nbsp;&nbsp;&nbsp;&nbsp;<i class='fa-solid fa-angle-right'></i> ";
+            switch($row['exercice']) {
+            	case "addsous":
+            		echo "Addition et soustraction";
+            		break;
+            	case "compl":
+            		echo "Compléments";
+            		break;
+            	case "multidiv":
+            		echo "Multiplication et division";
+            		break;
+            	case "prio":
+            		echo "Priorité des opérations";
+            		break;
+            	case "addrelatifs":
+            		echo "Nombres entiers relatifs";
+            		break;
+            	case "trous":
+            		echo "Calculs à trous";
+            		break;
+           }
+           echo	": ".$row['C']." fois<br>";
+        }
+        echo "<br>";
+        mysqli_free_result($result);
+    } else {
+        echo "No records matching your query were found.";
+    }
+} else {
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+}
 
-	mysqli_close($con);
+mysqli_close($con);
 ?>
 			</p>
 			</div>
--- admin/utilisateurs.php
+++ admin/utilisateurs.php
@@ -1,7 +1,5 @@
 <?php
-// We need to use sessions, so you should always start sessions using the below code.
 session_start();
-// If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin']) || $_SESSION['admin'] != 1) {
 	header('Location: ../login.html');
 	exit;
@@ -32,86 +30,85 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['admin'] != 1) {
 			<h2>Gestion des utilisateurs</h2>
 				<div>
 <?php
-	include '../mysql_login.php';
-	$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-	if (mysqli_connect_errno()) {
-		echo "Failed to connect to MySQL: ".mysqli_connect_error();
+include '../mysql_login.php';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: ".mysqli_connect_error();
+}
+
+// SI AJOUT D'UN USER
+if(isset($_GET['add']) && $_GET['add'] == 1) {
+	echo '<form action="utilisateurs.php" method="POST">';
+	echo '<i class="fa-solid fa-user-plus"></i>&nbsp;&nbsp;<input type="text" size="15" name="username" placeholder="Nom" id="username" required autofocus><br><br>';
+	echo '<i class="fa-solid fa-user-lock"></i>&nbsp;&nbsp;<input type="password" size="15" name="password" placeholder="Mot de passe" id="password" required><br><br>';
+	echo '<input type="submit" value="Créer">';
+	echo '</form>';
+	echo '<br><p style="text-align: center;"><i class="fa-solid fa-arrow-rotate-left"></i> <a href="utilisateurs.php">Retour</a></p>';
+
+// SI MODIFICATION D'UN USER
+} elseif (isset($_GET['edit']) && is_numeric($_GET['edit'])) {
+
+	$stmt = $con->prepare('SELECT username, password FROM accounts WHERE id = ?');
+	$stmt->bind_param('i', $_GET['edit']);
+	$stmt->execute();
+	$stmt->bind_result($username, $password);
+	$stmt->fetch();
+	$stmt->close();
+
+	echo '<form action="utilisateurs.php" method="POST">';
+	echo '<input type="hidden" name="id" id="id" value="'.$_GET['edit'].'">';
+	echo '<input type="hidden" name="oldpassword" id="oldpassword" value="'.$password.'">';		
+	echo '<i class="fa-solid fa-user-pen"></i>&nbsp;&nbsp;<input type="text" size="15" name="username" value="'.$username.'" id="username" required autofocus><br><br>';
+	echo '<i class="fa-solid fa-user-lock"></i>&nbsp;&nbsp;<input type="password" size="15" name="password" placeholder="*********" id="password"><br><br>';
+	echo '<input type="submit" value="Mettre à jour">';
+	echo '</form>';
+	echo '<br><p style="text-align: center;"><i class="fa-solid fa-arrow-rotate-left"></i> <a href="utilisateurs.php">Retour</a></p>';
+
+// LISTE DES USERS
+} else {
+
+	if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['id'])) {
+		if ($_POST['password'] !== "") $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+		else $password = $_POST['oldpassword'];
+		$stmt = "UPDATE accounts SET username='".$_POST['username']."', password='".$password."' WHERE id=".$_POST['id'];
+		mysqli_query($con, $stmt);
+	} elseif (isset($_POST['username']) && isset($_POST['password'])) {
+	  	$sql_u = "SELECT * FROM accounts WHERE username='".$_POST['username']."'";
+	  	$res_u = mysqli_query($con, $sql_u);
+	  	if (mysqli_num_rows($res_u) > 0) {
+	  	 	 echo "<b>Erreur</b><br>Nom d'utilisateur déjà pris";
+			echo '<br><br><p style="text-align: center;"><a href="utilisateurs.php">Retour</a></p>';
+	  	 	 exit();
+	  	} else {
+			$stmt = "INSERT INTO accounts (username, password) VALUES ('".$_POST['username']."', '".password_hash($_POST['password'], PASSWORD_DEFAULT)."')";
+			mysqli_query($con, $stmt);
+		}
 	}
 
-	// Si ajout d'un user
-	if(isset($_GET['add']) && $_GET['add'] == 1) {
-		echo '<form action="utilisateurs.php" method="POST">';
-		echo '<i class="fa-solid fa-user-plus"></i>&nbsp;&nbsp;<input type="text" size="15" name="username" placeholder="Nom" id="username" required autofocus><br><br>';
-		echo '<i class="fa-solid fa-user-lock"></i>&nbsp;&nbsp;<input type="password" size="15" name="password" placeholder="Mot de passe" id="password" required><br><br>';
-		echo '<input type="submit" value="Créer">';
-		echo '</form>';
-		echo '<br><p style="text-align: center;"><i class="fa-solid fa-arrow-rotate-left"></i> <a href="utilisateurs.php">Retour</a></p>';
-
-	// Si edit d'un user
-	} elseif (isset($_GET['edit']) && is_numeric($_GET['edit'])) {
-
-		$stmt = $con->prepare('SELECT username, password FROM accounts WHERE id = ?');
-		$stmt->bind_param('i', $_GET['edit']);
-		$stmt->execute();
-		$stmt->bind_result($username, $password);
-		$stmt->fetch();
-		$stmt->close();
-
-		echo '<form action="utilisateurs.php" method="POST">';
-		echo '<input type="hidden" name="id" id="id" value="'.$_GET['edit'].'">';
-		echo '<input type="hidden" name="oldpassword" id="oldpassword" value="'.$password.'">';		
-		echo '<i class="fa-solid fa-user-pen"></i>&nbsp;&nbsp;<input type="text" size="15" name="username" value="'.$username.'" id="username" required autofocus><br><br>';
-		echo '<i class="fa-solid fa-user-lock"></i>&nbsp;&nbsp;<input type="password" size="15" name="password" placeholder="*********" id="password"><br><br>';
-		echo '<input type="submit" value="Mettre à jour">';
-		echo '</form>';
-		echo '<br><p style="text-align: center;"><i class="fa-solid fa-arrow-rotate-left"></i> <a href="utilisateurs.php">Retour</a></p>';
-
-	// Page par défaut
+	if ($result = mysqli_query($con, "SELECT id, username FROM accounts ORDER BY id ASC")) {
+	    if (mysqli_num_rows($result) > 0) {
+	        echo "<table style='padding: 5px;border:0.5px solid black;margin-left:auto;margin-right:auto;'>";
+	            echo "<tr>";
+	                echo "<th style='text-align: left;'>Id</th>";
+	                echo "<th style='text-align: left;padding-left: 5px;'>Nom</th>";
+	                echo "<th></th>";
+	            echo "</tr>";
+	        while ($row = mysqli_fetch_array($result)) {
+	            echo "<tr>";
+	                echo "<td>" . $row['id'] . "</td>";
+	                echo "<td>" . $row['username'] . "</td>";
+	                echo "<td>&nbsp;&nbsp;<i class='fa-solid fa-user-pen'></i>&nbsp;<a href='utilisateurs.php?edit=". $row['id'] ."'>Editer</a></td>";
+	            echo "</tr>";
+	        }
+	        echo "</table><br>";
+	        mysqli_free_result($result);
+	    } else {
+	        echo "No records matching your query were found.";
+	    }
 	} else {
-
-		if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['id'])) {
-			if ($_POST['password'] !== "") $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-			else $password = $_POST['oldpassword'];
-			$stmt = "UPDATE accounts SET username='".$_POST['username']."', password='".$password."' WHERE id=".$_POST['id'];
-			mysqli_query($con, $stmt);
-		} elseif (isset($_POST['username']) && isset($_POST['password'])) {
-		  	$sql_u = "SELECT * FROM accounts WHERE username='".$_POST['username']."'";
-		  	$res_u = mysqli_query($con, $sql_u);
-		  	if (mysqli_num_rows($res_u) > 0) {
-		  	 	 echo "<b>Erreur</b><br>Nom d'utilisateur déjà pris";
-				echo '<br><br><p style="text-align: center;"><a href="utilisateurs.php">Retour</a></p>';
-		  	 	 exit();
-		  	} else {
-				$stmt = "INSERT INTO accounts (username, password) VALUES ('".$_POST['username']."', '".password_hash($_POST['password'], PASSWORD_DEFAULT)."')";
-				mysqli_query($con, $stmt);
-			}
-		}
-
-		if ($result = mysqli_query($con, "SELECT id, username FROM accounts ORDER BY id ASC")) {
-		    if (mysqli_num_rows($result) > 0) {
-		        echo "<table style='padding: 5px;border:0.5px solid black;margin-left:auto;margin-right:auto;'>";
-		            echo "<tr>";
-		                echo "<th style='text-align: left;'>Id</th>";
-		                echo "<th style='text-align: left;padding-left: 5px;'>Nom</th>";
-		                echo "<th></th>";
-		            echo "</tr>";
-		        while ($row = mysqli_fetch_array($result)) {
-		            echo "<tr>";
-		                echo "<td>" . $row['id'] . "</td>";
-		                echo "<td>" . $row['username'] . "</td>";
-		                echo "<td>&nbsp;&nbsp;<i class='fa-solid fa-user-pen'></i>&nbsp;<a href='utilisateurs.php?edit=". $row['id'] ."'>Editer</a></td>";
-		            echo "</tr>";
-		        }
-		        echo "</table><br>";
-		        // Free result set
-		        mysqli_free_result($result);
-		    } else {
-		        echo "No records matching your query were found.";
-		    }
-		} else {
-		    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
-		}
-		mysqli_close($con);
+	    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+	}
+	mysqli_close($con);
 ?>
 				<p style="text-align: center;"><i class="fa-solid fa-user-plus"></i> <a href="utilisateurs.php?add=1">Ajouter</p>
 <?php
