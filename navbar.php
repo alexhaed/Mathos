55182e714287d13ae9988b329827833f4e3ababb
Alexandre Haederli <alexhaederli@gmail.com>
2026-03-25 19:00:29 +0100

    feat: add password change link in navbar

--- navbar.php
+++ navbar.php
@@ -2,6 +2,7 @@
 			<div>
 				<h1><i class="fa-solid fa-calculator fa-1x"></i> Mathos</h1>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Quitter</a>
+				<a href="changermdp.php"><i class="fa-solid fa-key"></i>Mot de passe</a>
 				<a href="index.php"><i class="fas fa-user-circle"></i>Accueil</a>
 			</div>
 		</nav>
